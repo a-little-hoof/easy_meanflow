363e7d9e3a46c31450ba841fa57dbae10700e60c
Yifei <yw251@rice.edu>
2025-06-26 11:07:36 +0000

    fetch origin

--- exps/MF00/train_script.sh
+++ exps/MF00/train_script.sh
@@ -1,11 +1,3 @@
-<<<<<<< main
-#!/bin/bash    
-torchrun --standalone --nproc_per_node=8 train_mf.py \
-    --detach_tgt=1 \
-    --outdir=./image_experiments/mf/MF03 \
-    --data=./data/cifar10-32x32.zip \
-    --cond=0 --arch=ddpmpp --lr 10e-4 --batch 8
-=======
 export PYTORCH_ENABLE_FUNC_IMPL=1 && \
 export PYTORCH_DDP_NO_REBUILD_BUCKETS=1 && \
 export HF_HOME=/cpfs/user/weijian/cache && \
@@ -19,4 +11,3 @@ train_mf.py \
 --outdir=logs/mf/MF00 \
 --data=cifar10-32x32.zip \
 --cond=0 --arch=ddpmpp --lr 10e-4 --batch 256
->>>>>>> main
