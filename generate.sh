d2492d1c90658e89c5b3f5d908b13f41703bd986
Yifei <yw251@rice.edu>
2025-06-26 12:06:17 +0000

    update readme

--- generate.sh
+++ generate.sh
@@ -1,2 +1,2 @@
-python generate_meanflow.py --outdir=image_experiment/out --seeds=0-127 \
+python generate_onestep_mf.py --outdir=image_experiment/out --seeds=0-127 \
     --batch=128 --network='/path/to/your/network-snapshot.pkl'
